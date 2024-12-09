abd87a9a07632c3cc6be548cd745df1ce92fd021
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-09 02:48:18 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,120 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gameContainer = document.querySelector('.game-container');
-    const gameBoard = document.querySelector('.game-board');
-    const attemptsElement = document.querySelector('#attempts');
-    const newGameButton = document.querySelector('#new-game');
+  const newGameButton = document.querySelector('#new-game');
 
-class Card {
-  suit: string;
-  value: string;
+  class Card {
+    suit: string;
+    value: string;
 
-  constructor(suit: string, value: string) {
-    this.suit = suit;
-    this.value = value;
+    constructor(suit: string, value: string) {
+      this.suit = suit;
+      this.value = value;
+    }
+
+    toString(): string {
+      return `${this.value} of ${this.suit}`;
+    }
   }
 
-  toString(): string {
-    return `${this.value} of ${this.suit}`;
-  }
-}
+  class Game {
+    private deck: Card[];
+    private hand: Card[];
 
-class Game {
-  private deck: Card[];
-  private hand: Card[];
+    constructor() {
+      this.deck = this.generateDeck();
+      this.shuffleDeck();
+      this.hand = this.dealCards();
+    }
 
-  constructor() {
-    this.deck = this.generateDeck();
-    this.shuffleDeck();
-    this.hand = this.dealCards();
-  }
+    private generateDeck(): Card[] {
+      const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+      const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
+      const deck = [];
 
-  private generateDeck(): Card[] {
-    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    const deck = [];
+      for (let suit of suits) {
+        for (let value of values) {
+          deck.push(new Card(suit, value));
+        }
+      }
 
-    for (let suit of suits) {
-      for (let value of values) {
-        deck.push(new Card(suit, value));
+      return deck;
+    }
+
+    private shuffleDeck(): void {
+      for (let i = this.deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
       }
     }
 
-    return deck;
-  }
-
-  private shuffleDeck(): void {
-    for (let i = this.deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-    }
-  }
-
-  private dealCards(): Card[] {
-    const hand: Card[] = [];
-    for (let i = 0; i < 4; i++) {
-      const card = this.deck.pop();
-      if (card) {
-        hand.push(card);
+    private dealCards(): Card[] {
+      const hand: Card[] = [];
+      for (let i = 0; i < 4; i++) {
+        const card = this.deck.pop();
+        if (card) {
+          hand.push(card);
+        }
       }
+      return hand;
     }
-    return hand;
+
+    public renderGameBoard(cardRowElement: HTMLElement): void {
+      console.log('Rendering game board');
+      console.log('Card row element:', cardRowElement);
+      cardRowElement.innerHTML = '';
+      for (let i = 0; i < this.hand.length; i++) {
+        const card = this.hand[i];
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+    
+        if (i % 2 === 0) { // face down card
+          cardElement.classList.add('card-face-down');
+        } else { // face up card
+          cardElement.classList.add('card-face-up');
+          const cardFace = document.createElement('div');
+          cardFace.classList.add('card-face');
+          cardFace.innerHTML = `
+            <span class="card-suit">${card.suit}</span>
+            <span class="card-value">${card.value}</span>
+          `;
+          cardElement.appendChild(cardFace);
+        }
+    
+        console.log(`Adding card ${i + 1} to game board`);
+        console.log(cardElement);
+        cardRowElement.appendChild(cardElement);
+      }
+      console.log('Finished rendering game board');
+      console.log(cardRowElement);
+    }
   }
 
-  public renderGameBoard(boardElement: HTMLElement): void {
-    boardElement.innerHTML = '';
-    for (let i = 0; i < this.hand.length; i++) {
-      const card = this.hand[i];
-      const cardElement = document.createElement('div');
-      cardElement.classList.add('card');
-      cardElement.id = `card-${i + 1}`;
-      cardElement.innerHTML = card.toString();
-      boardElement.appendChild(cardElement);
-    }
+  console.log('Script is running');
+  const cardRow = document.querySelector('.card-row') as HTMLElement;
+  console.log('Card row element:', cardRow);
+
+  if (cardRow) {
+    console.log('Card row element found');
+    let game = new Game();
+    console.log('Game object created');
+    game.renderGameBoard(cardRow);
+  } else {
+    console.log('Card row element not found');
   }
-}
 
-let game = new Game();
-if (gameBoard) {
-  game.renderGameBoard(gameBoard);
-}
-
-if (newGameButton) {
-  newGameButton.addEventListener('click', () => {
-    game = new Game();
-    if (gameBoard) {
-      game.renderGameBoard(gameBoard);
-    }
-  });
-}
+  if (newGameButton) {
+    console.log('New game button found');
+    newGameButton.addEventListener('click', () => {
+      console.log('New game button clicked');
+      game = new Game();
+      console.log('Game object created');
+      if (cardRow) {
+        console.log('Card row element found');
+        game.renderGameBoard(cardRow);
+      } else {
+        console.log('Card row element not found');
+      }
+    });
+  } else {
+    console.log('New game button not found');
+  }
 });
